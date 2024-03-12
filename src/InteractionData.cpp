c0d9a5c22816672995a3091d9f4124c4b973b5d0
nitish-nayak <felarof.nayak@gmail.com>
2024-03-11 20:22:42 -0500

    add tritium handling for interaction chains

--- src/InteractionData.cpp
+++ src/InteractionData.cpp
@@ -61,14 +61,16 @@ namespace NeutrinoFluxReweight{
     InteractionData::Pt = Prod_P*sin_theta;
     InteractionData::Pz = Prod_P*cos_theta; 
     
-    if(Inc_pdg != 1000010020)Inc_Mass = particle->GetParticle(Inc_pdg)->Mass();
+    if((Inc_pdg != 1000010020) && (Inc_pdg != 1000010030))Inc_Mass = particle->GetParticle(Inc_pdg)->Mass();
+    else if(Inc_pdg == 1000010030) {Inc_Mass = 2.809;}
     else {Inc_Mass = 1.875;}
     
-    if(Prod_pdg != 1000010020)Prod_Mass = particle->GetParticle(Prod_pdg)->Mass();
+    if((Prod_pdg != 1000010020) && (Prod_pdg != 1000010030))Prod_Mass = particle->GetParticle(Prod_pdg)->Mass();
+    else if(Prod_pdg == 1000010030) {Prod_Mass = 2.809;}
     else{Prod_Mass = 1.875;}
      
-    InteractionData::Inc_Mass  = particle->GetParticle(Inc_pdg)->Mass();
-    InteractionData::Prod_Mass = particle->GetParticle(Prod_pdg)->Mass();
+    InteractionData::Inc_Mass  = Inc_Mass; // particle->GetParticle(Inc_pdg)->Mass();
+    InteractionData::Prod_Mass = Prod_Mass; // particle->GetParticle(Prod_pdg)->Mass();
 
     static const double NUCLEON_MASS = (particle->GetParticle(2112)->Mass() + particle->GetParticle(2212)->Mass())/2.;
     static const double NUCLEON_MASS2 = NUCLEON_MASS*NUCLEON_MASS;
